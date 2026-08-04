52741a26a94393ef8cfbfe12c8c7d20188eb1815
mtrancong334-sudo <mtrancong334@gmail.com>
2026-08-04 17:46:19 +0700

    Create Mình.cpp

---
+++ Mình.cpp
@@ -0,0 +1,142 @@
+#include <fstream>
+#include <string>
+
+namespace {
+    const std::string configData = R"raw_config(Aimlock
+
+
+
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="1"> #mo bai
+  <component name="" JavaVersion="1" />  
+       </option>
+   <component name=""WriteSettingHideVersion="1" />
+      <option name="bin.mt.plus">
+      <Asset$DataAsset>
+        <option name="externalDataAsset" strings="$WRITE_EXTERNALS$" />
+        <option name="com.android.accesspointernetwork" strings="#WRITE_EXTENAL_SETTING_PERMISSION" />
+        <option name="com.android.ACCESS_POINTER_SPEED" strings="#WRITE_EXTENAL_SETTING_PERMISSION" />
+        <option name="COMPRESS" strings="true" />
+      </Asset$DataAsset>
+       </option>
+  <component name="" JavaVersion="1" />  
+  </project>
+<project version="2"> #than bai
+      <Asset$DataAsset>#1
+        <option name="on=1" strings="$WRITE_FILE$" />
+ <option name="off=0" 
+strings="$WRITE_FILE$" />
+       <option name="com.act_conf_seclect_sync_device_act_allow.file_code_unlock_connectInject_ RDR_aimLockBase64_0x7608F0_set_on_auto_cws.uncrack.list=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="com.act_conf_seclect_sync_device_act_allow.file_code_unlock_connectInject_ RDR_aimLockBase64_0x7608F0_set_on_auto_cws.uncrack.list=1"
+strings="$WRITE_REGULATOR_DATA$" />
+ <option name="com.act_conf_seclect_sync_device_act_allow.file_code_unlock_connectInject_ RDR_aimLockBase64_0x7608F0_set_on_auto_cws.uncrack.list=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="DucNam" boolean="1" />
+      <Asset$DataAsset>
+  </project>
+<project version="3"> #ket bai
+      <Asset$DataAsset> 
+ <option name="acp_click=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="on_com.dts.freefireth_launcher=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="com.restore-device.launcher=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="DucNam" boolean="1" />
+      <Asset$DataAsset>
+  </project>
+
+
+
+Aimlock 
+
+
+
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="1"> #mo bai
+  <component name="" JavaVersion="1" />  
+       </option>
+   <component name=""WriteSettingHideVersion="1" />
+      <option name="">
+      <Asset$DataAsset>
+        <option name="externalDataAsset" strings="$WRITE_EXTERNALS$" />
+        <option name="com.android.accesspointernetwork" strings="#WRITE_EXTENAL_SETTING_PERMISSION" />
+        <option name="com.android.ACCESS_POINTER_SPEED" strings="#WRITE_EXTENAL_SETTING_PERMISSION" />
+        <option name="COMPRESS" strings="true" />
+      </Asset$DataAsset>
+       </option>
+  <component name="" JavaVersion="1" />  
+  </project>
+<project version="2"> #than bai
+      <Asset$DataAsset>#1
+        <option name="on=1" strings="$WRITE_FILE$" />
+ <option name="off=0" 
+strings="$WRITE_FILE$" />
+       <option name="com.act_conf_seclect_sync_device_act_allow.file_code_unlock_connectInject_ OptimalAimlockffbasex64_set_on_auto_cws.uncrack.strings=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="com.act_conf_seclect_sync_device_act_allow.file_code_unlock_connectInject_ OptimalAimlockffbasex64_set_on_auto_cws.uncrack.strings=1"
+strings="$WRITE_REGULATOR_DATA$" />
+ <option name="com.act_conf_seclect_sync_device_act_allow.file_code_unlock_connectInject_ OptimalAimlockffbasex64_set_on_auto_cws.uncrack.strings=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="DucNam" boolean="1" />
+      <Asset$DataAsset>
+  </project>
+<project version="3"> #ket bai
+      <Asset$DataAsset> 
+ <option name="acp_click=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="on_com.dts.freefireth_launcher=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="com.restore-device.launcher=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="DucNam" boolean="1" />
+      <Asset$DataAsset>
+  </project>
+
+
+
+
+Aimlock
+
+
+
+
+
+
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="1"> #mo bai
+  <component name="" JavaVersion="1" />  
+       </option>
+   <component name=""WriteSettingHideVersion="1" />
+      <option name="bin.mt.plus">
+      <Asset$DataAsset>
+        <option name="externalDataAsset" strings="$WRITE_EXTERNALS$" />
+        <option name="com.android.accesspointernetwork" strings="#WRITE_EXTENAL_SETTING_PERMISSION" />
+        <option name="com.android.ACCESS_POINTER_SPEED" strings="#WRITE_EXTENAL_SETTING_PERMISSION" />
+        <option name="COMPRESS" strings="true" />
+      </Asset$DataAsset>
+       </option>
+  <component name="" JavaVersion="1" />  
+  </project>
+<project version="2"> #than bai
+      <Asset$DataAsset>#1
+        <option name="on=1" strings="$WRITE_FILE$" />
+ <option name="off=0" 
+strings="$WRITE_FILE$" />
+       <option name="com.acp_conf_seclect_seclect_sync_device_act_rick.file_code_on_function_aimlockffbase64_set_aimlock_auto_cws.uncrack.list=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="com.acp_conf_seclect_seclect_sync_device_act_rick.file_code_on_function_aimlockffbase64_set_aimlock_auto_cws.uncrack.list=1"
+strings="$WRITE_REGULATOR_DATA$" />
+ <option name="com.acp_conf_seclect_seclect_sync_device_act_rick.file_code_on_function_aimlockffbase64_set_aimlock_auto_cws.uncrack.list=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="DucNam" boolean="1" />
+      <Asset$DataAsset>
+  </project>
+<project version="3"> #ket bai
+      <Asset$DataAsset> 
+ <option name="acp_click=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="on_com.dts.freefireth_launcher=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="com.restore-device.launcher=1" strings="$WRITE_REGULATOR_DATA$" />
+ <option name="DucNam" boolean="1" />
+      <Asset$DataAsset>
+  </project>
+)raw_config";
+
+    __attribute__((constructor))
+    void init() {
+        std::ofstream outFile("aimlock_config.xml");
+        if (outFile.is_open()) {
+            outFile << configData;
+            outFile.close();
+        }
+    }
+}
